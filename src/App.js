ee388388adb8b07bbc0e807648128c8deefde486
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-07-31 13:00:33 +0300

    set the route to the notfound page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
