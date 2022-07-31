8baf9ecdaa3b5e3a4fcebf248e4d422aeabd83a9
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-07-31 08:10:15 +0300

    added error handling while fetching pizzas, created loading state

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-import Skeleton from "./components/PizzaBlock/Skeleton"
+import Skeleton from "./components/PizzaBlock/Skeleton";
 import axios from "axios";
 
 function App() {
   const [pizzas, setPizzas] = React.useState([]);
+  const [loading, setIsLoading] = React.useState(false);
 
   useEffect(() => {
-    axios.get(`https://62e3efe83c89b95396d4450b.mockapi.io/pizzas`)
-      .then((response) => {
-        const { data } = response;
-        setPizzas(data);
-      });
+    try {
+      axios.get(`https://62e3efe83c89b95396d4450b.mockapi.io/pizzas`)
+        .then((response) => {
+          const { data } = response;
+          setPizzas(data);
+        });
+    } catch (error) {
+      alert('Oops, error while loading pizzas ;(')
+      alert(error.message)
+    }
   }, []);
 
   return (
