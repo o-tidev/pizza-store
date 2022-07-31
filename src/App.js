14df9d87a92d74e6b177087490a9c6599ea69870
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2022-07-31 13:11:21 +0300

    added cart route and component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React from "react";
 import Header from "./components/Header";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
+import Cart from "./Pages/Cart";
+
+
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +17,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
